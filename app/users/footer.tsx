ccca192fbd5bdb597828df77281adc2edc3f0f39
Amir Ghari <amir.ghari@metropolia.fi>
2024-09-25 15:23:32 +0300

    footer changed

---
+++ app/users/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>
+      <ul>
+        <li>Icon 1</li>
+        <li>footer 2</li>
+        <li>footer 3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default footer
