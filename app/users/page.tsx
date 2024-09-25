2dc37345838091b471c015a8291384341398587e
Amir Ghari <amir.ghari@metropolia.fi>
2024-09-25 14:50:02 +0300

    did

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const UsersPage = () => {
-  return <div>UsersPage</div>
+  return (
+    <div>
+      <ul>
+        <li>user 1</li>
+        <li>user 2</li>
+        <li>user 3</li>
+      </ul>
+    </div>
+  )
 }
 
 export default UsersPage
